b8a386fd4c0b47b6626a509e71899167c9515389
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-09 12:10:18 +0530

    new

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         char c1, c2;
-        c1 = '65';
+        c1 = '5';
         c2 = 'B';
         System.out.println("c1="+c1+" c2="+c2);
     }
