758375416f38491a9f2c5b3760edf431e3c3f9d9
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-09 12:40:23 +0530

    new

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
-        int a, i;
+        byte a, i;
 
         Scanner xe = new Scanner(System.in);
         System.out.println("give any number ");
         a = xe.nextByte();
-        for (i =a-1 ; i>0; i--) {
+        for (i =(byte)(a-1) ; i>0; i--) {
             a *= i;
         }
         System.out.println(a);
