4d2a5a5226edec1bf05ed3f957f82122512b17a1
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-09 12:07:24 +0530

    new

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        char c1, c2;
+        c1 = '65';
+        c2 = 'B';
+        System.out.println("c1="+c1+" c2="+c2);
+    }
+}
