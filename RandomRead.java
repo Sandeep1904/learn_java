4aea7e2649f5744b891ec58a3484442efdc2a6e5
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-16 11:36:33 +0530

    fahf

--- RandomRead.java
+++ RandomRead.java
@@ -9,7 +9,8 @@ class RandomRead {
         for (int i=0; i<10;i++){
             int n = random.nextInt();
             int m = random.nextInt();
-
+//            here ternary condition statement is used for the calculation of minimum random value
+//            we can also create a new method for this calculation and use it here by calling that method.
             int a = n>m ? m:n;
             System.out.println("first random number is "+ n);
             System.out.println("second random number is "+m);
