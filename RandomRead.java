39c98aff97ead8bcd9dcdda517dfb17bc6c8ac4f
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-16 11:23:06 +0530

    fahf

--- RandomRead.java
+++ RandomRead.java
@@ -4,16 +4,22 @@ import java.lang.Math;
 
 class RandomRead {
     public static void main(String[] args) {
-        double n = getRandomNumber();
-        double m = getRandomNumber();
+        Random random = new Random();
+        for (int i=0; i<10;i++){
+            int n = random.nextInt();
+            int m = random.nextInt();
+            int a = n>m ? m:n;
+            System.out.println(n);
+            System.out.println(m);
+            System.out.println(a);
+        }
 
-        double a = n>m ? m:n;
-        System.out.println(n);
-        System.out.println(m);
-        System.out.println(a);
+
+
+
     }
-    public static double getRandomNumber() {
-        double x = Math.random();
-        return x;
-    }
+//    public static double getRandomNumber() {
+//        double x = Math.random();
+//        return x;
+//    }
 }
