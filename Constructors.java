6db6c00462b468471473fe3eb3453dd1e980311b
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-22 17:38:28 +0530

    this prints max and min values using ternary operater

--- Constructors.java
+++ Constructors.java
@@ -2,7 +2,11 @@ class Constructors {
     public Constructors(){
         System.out.println("this is a default constructor");
     }
+    public Constructors(int a){
+        System.out.println("this is a parameterized constructor");
+    }
     public static void main(String[] args) {
-        Constructors cons = new Constructors();
+        Constructors cons0 = new Constructors();
+        Constructors cons1 = new Constructors(5);
     }
 }
