545ca1eb57796b2e1ee142d242396e732de68920
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-22 17:33:33 +0530

    default and paramerterized constructors

---
+++ Constructors.java
@@ -0,0 +1,8 @@
+class Constructors {
+    Constructors{
+        System.out.println("this is a constructor");
+    }
+    public static void main(String[] args) {
+        Constructors cons = new Constructors();
+    }
+}
