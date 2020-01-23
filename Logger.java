a5ce63570dc6b795014bb5872bf6f749927f50b6
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-23 12:24:22 +0530

    use of access modifiers

--- Logger.java
+++ Logger.java
@@ -6,13 +6,13 @@ public class Logger {
         System.out.println("Debug line: "+logLine);
     }
 }
-// Main.java
-package package2;
-        import package1.Logger;
-public class Main extends Logger {
-    public static void main(String [] args){
-        Main logger = new Main();
-        // invokes debug() from Logger class
-        logger.debug("hello from main");
-    }
-}
+// Main1.java
+//package package2;
+//        import package1.Logger;
+//public class Main1 extends Logger {
+//    public static void main(String [] args){
+//        Main1 logger = new Main1();
+//        // invokes debug() from Logger class
+//        logger.debug("hello from main");
+//    }
+//}
