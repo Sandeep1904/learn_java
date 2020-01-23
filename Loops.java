8a3b19848912c7147c68dc7b92a8cf9e48eb2645
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-23 12:32:44 +0530

    use of different type of loops

--- Loops.java
+++ Loops.java
@@ -5,6 +5,14 @@ class Loops {
         // basic for loop!
         for (int i = 0; i < vowels.length; ++ i) {
             System.out.print(vowels[i]+ " ");
+
         }
+        System.out.println();
+        // using for-each loop!
+        for (char item: vowels) {
+            System.out.print(item+ " ");
+
+        }
+        System.out.println();
     }
 }
