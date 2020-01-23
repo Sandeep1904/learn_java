9fd505bf47684b28ad4d327b18a85c95e8c70018
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-23 12:15:57 +0530

    use of access modifiers

--- Access.java
+++ Access.java
@@ -6,5 +6,20 @@ public class Access {
 
     public static void main(String[] args) {
         message();
+        Data d = new Data();
+        d.setName("Programiz");
+        System.out.println(d.getName());
+    }
+}
+
+//this is an example of private access modifiers
+class Data {
+    private String name;
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name1) {
+        name = name1;
+
     }
 }
