cb2c3382dfeb447e6444aa454293b267807b53a5
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-23 12:28:25 +0530

    use of access modifiers

---
+++ Loops.java
@@ -0,0 +1,10 @@
+class Loops {
+    public static void main(String[] args) {
+
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        // basic for loop!
+        for (int i = 0; i < vowels.length; ++ i) {
+            System.out.print(vowels[i]+ " ");
+        }
+    }
+}
