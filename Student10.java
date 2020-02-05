ec2dd114ff51db845e44121cb524db3f31d43e1f
Sandeep1904 <sandeephugar19@gmail.com>
2020-02-05 16:27:01 +0530

    djf

--- Student10.java
+++ Student10.java
@@ -1,13 +1,17 @@
+import java.util.*;
 class Student10{
     static int roll;
     String name;
 
     static void update()
     {
-        System.out.println(roll);
+        System.out.println("the roll no of the student is " + roll);
     }
     public static void main(String args[])
     {
+        Scanner xe = new Scanner(System.in);
+        roll = xe.nextInt();
+        Student10 student10 = new Student10();
         student10.update();
     }
 }
