d9c8f611142f37e2fd3bc717c02c0ff33e4ad367
Sandeep1904 <sandeephugar19@gmail.com>
2020-02-05 16:51:00 +0530

    djf

---
+++ Student2.java
@@ -0,0 +1,8 @@
+class Student2{
+    static{
+        System.out.println("static block is invoked");
+    }
+    public static void main(String args[]){
+        System.out.println("Hello main");
+    }
+}
