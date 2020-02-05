9de6bf6a6a614e0cc8b36850305c87473aa4a2b4
Sandeep1904 <sandeephugar19@gmail.com>
2020-02-05 16:45:46 +0530

    djf

---
+++ Student1.java
@@ -0,0 +1,13 @@
+class Student1{
+    static int roll;
+    String name;
+    static void update()
+    {
+        System.out.println(roll);
+    }
+
+    public static void main(String args[])
+    {
+        Student1.update();
+    }
+}
