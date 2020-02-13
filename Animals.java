aff235513f9b944841de990f84a87bdb76157a77
Sandeep1904 <sandeephugar19@gmail.com>
2020-02-13 11:32:26 +0530

    inheritance

---
+++ Animals.java
@@ -0,0 +1,25 @@
+class Animals {
+    void eat(){
+        System.out.println("eating");
+    }
+}
+class Dog extends Animals{
+    void bark(){
+        System.out.println("barking");
+    }
+}
+class Babydog extends Dog {
+    void weep() {
+        System.out.println("weeping");
+
+    }
+}
+class TestInheritance1{
+    public static void main(String[] args) {
+        Babydog d = new Babydog();
+        d.weep();
+        d.bark();
+        d.eat();
+
+    }
+}
